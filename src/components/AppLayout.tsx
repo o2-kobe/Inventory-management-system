ff00c12f45979d07c4603eb32ac8665a689f3543
Laud Tetteh <kobbykurtx@gmail.com>
2025-09-30 13:06:52 +0000

    chore(layout): minor layout updates

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -5,7 +5,7 @@ const AppLayout = () => {
   return (
     <div className="grid grid-cols-[16rem_1fr] gap-3">
       <SideMenu />
-      <div className="mt-10 px-10">
+      <div className="my-10 px-10">
         <Outlet />
       </div>
     </div>
