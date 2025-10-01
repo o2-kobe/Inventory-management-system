5da2e9fa6257938df96e962c3d5b5973b8ccd97e
Laud Tetteh <kobbykurtx@gmail.com>
2025-10-01 14:43:49 +0000

    fix(court-type): update device links and UI tweaks

--- src/components/CourtType.tsx
+++ src/components/CourtType.tsx
@@ -15,8 +15,16 @@ const CourtType = () => {
 
   return (
     <div>
-      <h3 className="font-semibold text-4xl mb-2">{heading}</h3>
-      <p className="">Select a court to manage devices</p>
+      <div className="flex items-center justify-between">
+        <div>
+          <h3 className="font-semibold text-4xl mb-2">{heading}</h3>
+          <p className="">Select a court to manage devices</p>
+        </div>
+
+        <button className="px-4 cursor-pointer text-sm transition-colors duration-[0.2s] hover:bg-green-700 py-2 rounded-md bg-green-800 text-white">
+          Add New Court
+        </button>
+      </div>
 
       <ul className="mt-4 text-blue-800 space-y-2">
         {(courtType?.courts ?? []).map((court) => (
