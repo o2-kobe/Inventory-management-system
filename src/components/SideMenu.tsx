dd6e8748aef0b9554a1256c13bf2a6f51efd4b48
Laud Tetteh <kobbykurtx@gmail.com>
2025-10-01 14:43:51 +0000

    chore(nav): minor styling/structure updates

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -5,47 +5,49 @@ import RegionsRoute from "./RegionsRoute";
 
 const SideMenu = () => {
   return (
-    <div className="px-4 py-8 border border-r-gray-300 shadow-md border-y-white min-h-screen flex flex-col">
-      <div>
-        <div className="flex flex-col justify-center items-center gap-1">
-          <div className="bg-white w-[80px]">
-            <img src="/logo1.webp" alt="logo" className="bg-white" />
+    <div className="px-4 py-8 border border-r-gray-300 shadow-md border-y-white hide-scrollbar overflow-y-auto flex flex-col">
+      <div className="flex flex-col justify-between h-full">
+        <div>
+          <div className="flex flex-col justify-center items-center gap-1">
+            <div className="bg-white w-[80px]">
+              <img src="/logo1.webp" alt="logo" className="bg-white" />
+            </div>
+
+            {/* Heading */}
+            <div className="font-semibold mb-4">
+              <p>Inventory Management </p>
+              <p className="text-center mx-auto px-auto">System</p>
+            </div>
           </div>
 
-          {/* Heading */}
-          <div className="font-semibold mb-4">
-            <p>Inventory Management </p>
-            <p className="text-center mx-auto px-auto">System</p>
+          {/* NavLinks */}
+          <div className="text-center mt-5 flex flex-col gap-2">
+            <NavLink
+              to="/app/dashboard"
+              className={({ isActive }) =>
+                ` flex gap-1 items-center font-semibold px-3 py-2  ${
+                  isActive ? "bg-gray-200 rounded-md" : ""
+                }`
+              }
+            >
+              <MdOutlineDashboard />
+              Dashboard
+            </NavLink>
+
+            <RegionsRoute />
           </div>
         </div>
-
-        {/* NavLinks */}
-        <div className="text-center mt-5 flex flex-col gap-2">
-          <NavLink
-            to="/app/dashboard"
-            className={({ isActive }) =>
-              ` flex gap-1 items-center font-semibold px-3 py-2  ${
-                isActive ? "bg-gray-200 rounded-md" : ""
-              }`
-            }
-          >
-            <MdOutlineDashboard />
-            Dashboard
-          </NavLink>
-
-          <RegionsRoute />
-        </div>
+        <NavLink
+          to="/app/settings"
+          className={({ isActive }) =>
+            ` flex gap-1 items-center font-semibold px-3 py-2 mt-auto  ${
+              isActive ? "bg-gray-200 rounded-md" : ""
+            }`
+          }
+        >
+          <MdOutlineSettings /> Settings
+        </NavLink>
       </div>
-      <NavLink
-        to="/app/settings"
-        className={({ isActive }) =>
-          ` flex gap-1 items-center font-semibold px-3 py-2 mt-auto  ${
-            isActive ? "bg-gray-200 rounded-md" : ""
-          }`
-        }
-      >
-        <MdOutlineSettings /> Settings
-      </NavLink>
     </div>
   );
 };
