c0ea641fdf40bf73dc0eea1af92d938dbb072439
Laud Tetteh <kobbykurtx@gmail.com>
2025-10-01 14:43:50 +0000

    chore(nav): regions submenu and link tweaks

--- src/components/RegionsRoute.tsx
+++ src/components/RegionsRoute.tsx
@@ -2,7 +2,7 @@ import { FiMap } from "react-icons/fi";
 import { MdExpandMore } from "react-icons/md";
 import { regionsData } from "../data";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const RegionsRoute = () => {
   const [viewRegions, setViewRegions] = useState(false);
@@ -10,7 +10,7 @@ const RegionsRoute = () => {
   return (
     <div
       className="font-semibold px-3 py-2"
-      onClick={() => setViewRegions((r) => !r)}
+      onClick={() => setViewRegions(true)}
     >
       <div className="flex items-center gap-1 mb-1 cursor-pointer">
         {viewRegions ? <MdExpandMore /> : <FiMap />}Regions
@@ -19,16 +19,20 @@ const RegionsRoute = () => {
       <div
         className={`${
           viewRegions ? "" : "hidden"
-        } flex flex-col gap-2 items-start ml-6`}
+        } flex flex-col  items-start ml-6`}
       >
         {regionsData.map((region, i) => (
-          <Link
+          <NavLink
             to={`/app/regions/${region.slug}`}
-            className="cursor-pointer"
+            className={({ isActive }) =>
+              ` flex gap-1 items-center font-semibold px-3 py-1  ${
+                isActive ? "bg-gray-200 rounded-md" : ""
+              }`
+            }
             key={i}
           >
             {region.regionName}
-          </Link>
+          </NavLink>
         ))}
       </div>
     </div>
