bbea626d7f49f3951e90d2f5c9d4b03ce94f05a7
Laud Tetteh <kobbykurtx@gmail.com>
2025-10-01 14:43:48 +0000

    chore(layout): minor layout adjustments

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -3,9 +3,9 @@ import SideMenu from "./SideMenu";
 
 const AppLayout = () => {
   return (
-    <div className="grid grid-cols-[16rem_1fr] gap-3">
+    <div className="grid grid-cols-[16rem_1fr] gap-3 h-screen">
       <SideMenu />
-      <div className="my-10 px-10">
+      <div className="my-10 px-10 overflow-y-auto hide-scrollbar">
         <Outlet />
       </div>
     </div>
