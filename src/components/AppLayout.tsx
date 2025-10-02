9552bb99681576b3e857f2059035f67c88533262
Laud Tetteh <kobbykurtx@gmail.com>
2025-10-02 20:58:57 +0000

    chore(layout): layout structure updates

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,14 +1,20 @@
 import { Outlet } from "react-router-dom";
 import SideMenu from "./SideMenu";
+import { ModalProvider } from "../contexts/ModalContext";
 
 const AppLayout = () => {
   return (
-    <div className="grid grid-cols-[16rem_1fr] gap-3 h-screen">
-      <SideMenu />
-      <div className="my-10 px-10 overflow-y-auto hide-scrollbar">
-        <Outlet />
+    <ModalProvider>
+      <div className="grid grid-cols-[16rem_1fr] gap-3 h-screen">
+        <SideMenu />
+        <div
+          id="main-content"
+          className="my-10 px-10 overflow-y-auto hide-scrollbar"
+        >
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </ModalProvider>
   );
 };
 export default AppLayout;
