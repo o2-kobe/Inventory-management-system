184c45888695484905fdf9c2c00a700a2d6ec4f5
Laud Tetteh <kobbykurtx@gmail.com>
2025-10-02 20:58:58 +0000

    feat(departments): department devices UI and functionality improvements

--- src/components/DepartmentDevices.tsx
+++ src/components/DepartmentDevices.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { getDepartmentByIds } from "../dataRepo";
 import Table from "./Table";
+import CreateNewButton from "./CreateNewButton";
 
 const DepartmentDevices = () => {
   const { regionSlug, departmentId } = useParams();
@@ -22,9 +23,7 @@ const DepartmentDevices = () => {
           </p>
         </div>
 
-        <button className="px-4 cursor-pointer text-sm transition-colors duration-[0.2s] hover:bg-green-700 py-2 rounded-md bg-green-800 text-white">
-          Add new Equipment
-        </button>
+        <CreateNewButton>Add new Device</CreateNewButton>
       </div>
 
       <Table devices={devices} />
