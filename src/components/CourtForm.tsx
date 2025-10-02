6cbe4ee4611d0bfec56822516e5af1c487d706f8
Laud Tetteh <kobbykurtx@gmail.com>
2025-10-02 20:59:00 +0000

    feat(forms): add CourtForm component

---
+++ src/components/CourtForm.tsx
@@ -0,0 +1,25 @@
+import CreateNewButton from "./CreateNewButton";
+import Input from "./Input";
+
+const CourtForm = () => {
+  return (
+    <form>
+      <h3 className="font-semibold text-3xl mb-3">Register a new Court</h3>
+      <div className="flex flex-col gap-5">
+        <div className="flex gap-15">
+          <Input label="Court Name" placeholder="eg. Tema Circuit Court" />
+        </div>
+        <div className="flex gap-15">
+          <Input label="RegionId" placeholder="eg. ejhsghisf8" />
+        </div>
+        <div className="flex gap-15">
+          <Input label="CourtType Id" placeholder="eg. ejhsfg7is" />
+        </div>
+      </div>
+      <div className="flex justify-end">
+        <CreateNewButton>Submit</CreateNewButton>
+      </div>
+    </form>
+  );
+};
+export default CourtForm;
